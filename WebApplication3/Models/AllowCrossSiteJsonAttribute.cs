9d2a7ea6a06f9050bf71f51cf1519f695808f9fb
weihang chen <wech@syntronic.com>
2019-04-03 23:53:38 +0200

    Add project files.

---
+++ WebApplication3/Models/AllowCrossSiteJsonAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication3.Models
+{
+    //important! keep this, else local test wont work
+    public class AllowCrossSiteJsonAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
